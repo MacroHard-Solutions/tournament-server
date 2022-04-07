9c308c69cd8e2e1f8e600dfc180d4f052d5f8036
Kian Reddy <kianreddy67@gmail.com>
2022-04-07 23:13:28 +0200

    UI Initialization

---
+++ src/pages/Notfound.js
@@ -0,0 +1,13 @@
+import '../styles/Notfound.css'
+
+function Notfound() {
+
+    return(
+        <div className='Error'>
+            <iframe title='NotFoundGif' src="https://giphy.com/embed/8L0Pky6C83SzkzU55a" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/mini-italia-8L0Pky6C83SzkzU55a">via GIPHY</a></p>
+            <h1>ERROR 404: Not found</h1>
+        </div>
+    );
+}
+
+export default Notfound;
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
-function Profile() {
+function Profile({login}) {
+
+    const history = useHistory();
+
+    useEffect(() => {
+        if(!login){
+            history.push('/playerverification')
+        }
+    }, [history,login]);
+
     return(
         <h1>Profile</h1>
     )
