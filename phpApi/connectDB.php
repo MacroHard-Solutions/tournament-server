ceca10d07e035946e0f9e6ade816d6ab1e900ce4
moose918 <gumpu.musa@gmail.com>
2022-04-17 13:48:51 +0200

    Allows for inserting into Game table Returns json object of the new row

--- phpApi/connectDB.php
+++ phpApi/connectDB.php
@@ -1,4 +1,6 @@
 <?php
+require ("insertToDB.php");
+//require __DIR__."";
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
@@ -13,50 +15,70 @@ $GLOBALS["YES"] = json_encode("YES");
 $GLOBALS["NO"] = json_encode("NO");
 
 /**
- * Initiates a connectino to the database
+ * Initiates a connection to the database
  *
  * @return mysqli
  */
 function connectToDB(): mysqli
 {
-	$db = new mysqli(
-		"tourney-server-database.c2ncyvtifq7i.us-east-1.rds.amazonaws.com",
-		"root",
-		"44gRmEvP9xLhR6d",
-		"db_tourney_server",
-		"3306"
-	);
-	// $db = new mysqli(
-	// 	"localhost",
-	// 	"root",
-	// 	"",
-	// 	"db_tourney_server",
-	// 	"3306"
-	// );
+  $db = new mysqli(
+    "tourney-server-database.c2ncyvtifq7i.us-east-1.rds.amazonaws.com",
+    "root",
+    "44gRmEvP9xLhR6d",
+    "db_tourney_server",
+    "3306"
+  );
+  // For connecting to a local DB
+  // $db = new mysqli(
+  // 	"localhost",
+  // 	"root",
+  // 	"",
+  // 	"db_tourney_server",
+  // 	"3306"
+  // );
 
-	$db->set_charset("utf8");
+  // if the connection was unsuccessful
+  if ($db->connect_errno)
+  {
+    echo ($GLOBALS["ERROR"].": Unable to connect to DB");
+    exit($GLOBALS['ERROR']);
+  }
 
-	return $db;
+  $db->set_charset("utf8");
+
+  return $db;
 }
 
 /**
- * Checks if executing a statement was successful.
- * 
- * If not, the program needs to end
- * 
- * @param mysqli $db The database
- * @param bool $success Whether the execution was successful
- * 
+ * Checks if executing a statement was successful
+ * @param mysqli $db The connected database
+ * @param bool $successful
  * @return void
  */
-function checkStatementFailure(mysqli $db, bool $success)
+function checkStatementFailure(mysqli $db, bool $successful)
 {
-	if ($success == false) {
-		$db->close();
-		exit($GLOBALS["ERROR"]);
-	}
+  if (!$successful) {
+    $db->close();
+    exit($GLOBALS["ERROR"]);
+  }
 }
 
 $db = connectToDB();
 
-phpinfo();
+switch ($_POST['function']){
+  case "signup":
+    insertUser($db);
+    break;
+
+  case "insert":
+    insertToTable($db);
+    break;
+
+  default:
+    echo ($GLOBALS["ERROR"]);
+
+}
+
+// list functionality
+
+$db->close();
--- phpApi/insertToDB.php
+++ phpApi/insertToDB.php
@@ -1,159 +1,194 @@
 <?php
-require __DIR__ . "/connectDB.php";
+/**
+ * @param mysqli $db
+ * @return bool
+ */
+function insertUser(mysqli $db)
+{
+  $stmt = $db->prepare("CALL insert_user(?, ?, ?, ?, ?, ?, ?, ?)");
+  $stmt->bind_param(
+    'ssssssii',
+    $_POST['userID'],
+    $_POST['fName'],
+    $_POST['lName'],
+    $_POST['userName'],
+    $_POST['userEmail'],
+    $_POST['userPass'],
+    $_POST['isAdmin'],
+    $_POST['wantsNotifications']
+  );
+
+  checkStatementFailure($db, $stmt->execute());
+
+  return true;
+}
 
 /**
- * Inserts user-defined attributes.
- * 
- * In the POST array, the following attributes are retrieved:
- * 
- * userID , fName, lName, userName, userEmail, userPass, isAdmin, wantsNotificaitons
- * 
- * @param mysqli $db The active database
- * @return bool Indicates that the insert was successful
+ * @param mysqli $db
+ * @param string $addressID The new addressID obtained to associate with the agent
+ * @return string The newly inserted UUID
  */
-function insertUser($db)
+function insertAgent(mysqli $db, string $addressID) : string
 {
-	$stmt = $db->prepare("CALL insert_user(?, ?, ?, ?, ?, ?, ?, ?)");
-	$stmt->bind_param(
-		'ssssssii',
-		$_POST['userID'],
-		$_POST['fName'],
-		$_POST['lName'],
-		$_POST['userName'],
-		$_POST['userEmail'],
-		$_POST['userPass'],
-		$_POST['isAdmin'],
-		$_POST['wantsNotifications']
-	);
+  // echo("  ".$addressID ." is the ID");
+  $stmt = $db->prepare("CALL insert_agent(?, ?, ?, ?)");
+  $stmt->bind_param(
+    'sssd',
+    $_POST['userID'],
+    $_POST['addressID'],//$addressID,
+    $_POST['tournamentID'],
+    $_POST['agentELO']
+  );
 
-	checkStatementFailure($db, $stmt->execute());
+  checkStatementFailure($db, $stmt->execute());
 
-	return true;
+  // get agentID from previous insertion
+  $result = $db->query("CALL get_latest_agent_id()");
+  return $result->fetch_assoc()["USER_AGENT_ID"];
 }
-function insertAgent($db, $addressID)
+
+/**
+ * @param mysqli $db
+ * @return string The newly inserted UUID
+ */
+function insertAddress(mysqli $db) : string
 {
-	// echo("  ".$addressID ." is the ID");
-	$stmt = $db->prepare("CALL insert_agent(?, ?, ?, ?)");
-	$stmt->bind_param(
-		'sssd',
-		$_POST['userID'],
-		$_POST['addressID'],//$addressID,
-		$_POST['tournamentID'],
-		$_POST['agentELO']
-	);
+  $stmt = $db->prepare("CALL insert_agent_address( ?, ?)");
+  $stmt->bind_param('si', $_POST['addressIP'], $_POST['portNum']);
 
-	checkStatementFailure($db, $stmt->execute());
+  checkStatementFailure($db, $stmt->execute());
+  $stmt->store_result();
 
-	// get agentID from previous insertion
-	$result = $db->query("CALL get_latest_agent_id()");
-	return $result->fetch_assoc()["USER_AGENT_ID"];
+  // get addressID from previous insertion
+  $result = $db->query("CALL get_latest_address_id();");
+  $newAddressID = $result->fetch_assoc()["ADDRESS_ID"];
+  $result->free();
+
+  $db->next_result();
+
+  return $newAddressID;
 }
-function insertAddress($db)
+
+/**
+ * @param mysqli $db
+ * @return string The newly inserted UUID
+ */
+function insertGame(mysqli $db) :string
 {
-	$stmt = $db->prepare("CALL insert_agent_address( ?, ?)");
-	$stmt->bind_param('si', $_POST['addressIP'], $_POST['portNum']);
+  $stmt = $db->prepare("CALL insert_game(?, ?)");
+  $stmt->bind_param('ss', $_POST['gameName'], $_POST['fileName']);
 
-	checkStatementFailure($db, $stmt->execute());
-	$stmt->store_result();
+  checkStatementFailure($db, $stmt->execute());
 
-	// get addressID from previous insertion
-	$result = $db->query("CALL get_latest_address_id();");
-	$newAddressID = $result->fetch_assoc()["ADDRESS_ID"];
-	$result->free();
+  $result = $stmt->get_result();
+  $currRow = $result->fetch_assoc();
+  echo (json_encode($currRow));
+  $result->free();
 
-	$db->next_result();	
-
-	return $newAddressID;
+  // get gameID from previous insertion
+//  $result = $db->query("CALL get_latest_game_id()");
+  return json_encode($currRow);
 }
-function insertGame($db)
+
+/**
+ * @param mysqli $db
+ * @return string The newly inserted UUID
+ */
+function insertTournament(mysqli $db) :string
 {
-	$stmt = $db->prepare("CALL insert_game(?, ?)");
-	$stmt->bind_param('sb', $_POST['gameName'], $_POST['fileName']);
+  $stmt = $db->prepare("CALL insert_tournament(?, ?)");
+  $stmt->bind_param('ss', $_POST['tournamentName'], $_POST['gameID']);
 
-	checkStatementFailure($db, $stmt->execute());
+  checkStatementFailure($db, $stmt->execute());
 
-	// get gameID from previous insertion
-	$result = $db->query("CALL get_latest_game_id()");
-	return $result->fetch_assoc()["GAME_ID"];
+  // get agentID from previous insertion
+  $result = $db->query("CALL get_latest_tournament_id()");
+  return $result->fetch_assoc()["TOURNAMENT_ID"];
 }
-function insertTournament($db)
+
+/**
+ * @param mysqli $db
+ * @return string The newly inserted UUID
+ */
+function insertMatchLog(mysqli $db) : string
 {
-	$stmt = $db->prepare("CALL insert_tournament(?, ?)");
-	$stmt->bind_param('ss', $_POST['tournamentName'], $_POST['gameID']);
+  $stmt = $db->prepare("CALL insert_match_log(?, ?, ?)");
+  $stmt->bind_param('sss', $_POST['tournamentID'], $_POST['matchLogTime'], $_POST['gameLog']);
 
-	checkStatementFailure($db, $stmt->execute());
+  checkStatementFailure($db, $stmt->execute());
 
-	// get agentID from previous insertion
-	$result = $db->query("CALL get_latest_tournament_id()");
-	return $result->fetch_assoc()["TOURNAMENT_ID"];
+  // get agentID from previous insertion
+  $result = $db->query("CALL get_latest_match_log_id()");
+  return $result->fetch_assoc()["MATCH_LOG_ID"];
 }
-function insertMatchLog($db)
+
+// Need to figure out how matches are obtained
+/**
+ * @param mysqli $db
+ * @param string $matchLogID  The related match log ID
+ * @return bool
+ */
+function insertRanking(mysqli $db, string $matchLogID) : bool
 {
-	$stmt = $db->prepare("CALL insert_match_log(?, ?, ?)");
-	$stmt->bind_param('sss', $_POST['tournamentID'], $_POST['matchLogTime'], $_POST['gameLog']);
+  $stmt = $db->prepare("CALL insert_ranking(?, ?, ?)");
+  $stmt->bind_param('ssi', $_POST['matchLogID'], $_POST['agentID'], $_POST['ranking']);
 
-	checkStatementFailure($db, $stmt->execute());
+  checkStatementFailure($db, $stmt->execute());
 
-	// get agentID from previous insertion
-	$result = $db->query("CALL get_latest_match_log_id()");
-	return $result->fetch_assoc()["MATCH_LOG_ID"];
-}
-function insertRanking($db, $matchLogID)
-{
-	$stmt = $db->prepare("CALL insert_ranking(?, ?, ?)");
-	$stmt->bind_param('ssi', $_POST['matchLogID'], $_POST['agentID'], $_POST['ranking']);
-
-	checkStatementFailure($db, $stmt->execute());
-
-	return true;
+  return true;
 }
 
-$db = connectToDB();
+/**
+ * @param mysqli $db
+ * @return void
+ */
+function insertToTable(mysqli $db){
+  $result = null;
 
-function insertToTable($insertType, ){
+  switch ($_POST["insertType"]) {
+    case "user":
+      // insert user
+      $result = insertUser($db);
+      break;
+    case "game":
+      // insert game
+      $newID = insertGame($db);
+      break;
+    case "tournament":
+      // insert tournament
+      $newID = insertTournament($db);
+      break;
+    case "agent":
+      // insert agent and associated address
+      $addressID = insertAddress($db);
+
+      //
+      echo ("AddressID before Agent entry is " . $addressID);
+
+      // TODO: FIX
+      break;
+    case "addressAgent":
+      $newID = insertAgent($db, 0);//$addressID);
+      break;
+
+      break;
+    case "matchLog":
+      // inserts match log and rankings
+      $matchLogID = insertMatchLog($db);
+      echo ("MatchLogID before multiple rankings entries is " . $matchLogID);
+      insertRanking($db, $matchLogID); // TODO: There's multiple to account for
+      break;
+    default:
+      echo ($GLOBALS["ERROR"]);
+
+
+      if (isset($newID))
+        echo ("New ID for data entry is " . $newID);
+      else if ($result)
+        echo ($GLOBALS["SUCCESS"]);
+      else
+        echo ($GLOBALS["ERROR"]);
+  }
+
 
 }
-switch ($_POST["insertType"]) {
-	case "user":
-		// insert user
-		$result = insertUser($db);
-		break;
-	case "game":
-		// insert game
-		$newID = insertGame($db);
-		break;
-	case "tournament":
-		// insert tournament
-		$newID = insertTournament($db);
-		break;
-	case "agent":
-		// insert agent and address
-		$addressID = insertAddress($db);
-
-		//
-		echo ("AddressID before Agent entry is " . $addressID);
-	
-	// TODO: FIX
-	case "addressAgent":
-		$newID = insertAgent($db, 0);//$addressID);
-		break;
-
-		break;
-	case "matchLog":
-		// inserts match log and rankings
-		$matchLogID = insertMatchLog($db);
-		echo ("MatchLogID before multiple rankings entries is " . $matchLogID);
-		insertRanking($db, $matchLogID); // TODO: There's multiple to account for
-		break;
-	default:
-		echo ($GLOBALS["ERROR"]);
-}
-
-if (isset($newID))
-	echo ("New ID for data entry is " . $newID);
-else if ($result)
-	echo ($GLOBALS["SUCCESS"]);
-else
-	echo ($GLOBALS["ERROR"]);
-
-$db->close();
