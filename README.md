4e6622df90caf1517746e96732dd0a492a2e12f2
moose918 <gumpu.musa@gmail.com>
2022-04-24 04:44:40 +0200

    api folder created

--- README.md
+++
@@ -1,1 +0,0 @@
-#DB APIThis API provides access to the database which manages and stores all tournament server data.##Usage### To
--- routes/homeRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const homeController = require('../controllers/homeController');
-
-const router = express.Router();
-
-router.route('/').get(homeController.welcome);
-
-module.exports = router;
