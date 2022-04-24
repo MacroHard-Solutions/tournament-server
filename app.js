4a5a13e7136f19b28af5e3b512f7c1ab093b1cf9
moose918 <gumpu.musa@gmail.com>
2022-04-24 04:23:30 +0200

    changed to npm CORS package

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express'); // Framework for node
 const morgan = require('morgan'); // HTTP request logger
+const cors = require('cors'); // CORS middleware
 
 const slugify = require('slugify'); // NOTE: This is random...
 
@@ -15,36 +16,37 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev')); // Debug logging
 }
 
+app.use(cors());
+// app.use((req, res, next) => {
+//   // Website you wish to allow to connect
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+//   );
+
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'X-Requested-With,content-type'
+//   );
+
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   // res.setHeader('Access-Control-Allow-Credentials', true);
+
+//   // Pass to next layer of middleware
+//   next();
+// });
+
 app.use(express.json()); // Parse the body into json
 app.use(express.static(`${__dirname}/public`)); // Provide the /public directory for file transfer
 
 // Log the time the file was requested
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  next();
-});
-
-app.use((req, res, next) => {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type'
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  // res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
   next();
 });
 
