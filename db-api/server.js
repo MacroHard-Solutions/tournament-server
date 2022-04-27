1ddb75ea0047ee13f76f1d98477d9a7e4feb44ad
Musa <gumpu.musa@gmial.com>
2022-04-27 17:16:09 +0200

    changed server port

--- db-api/server.js
+++ db-api/server.js
@@ -3,7 +3,7 @@ const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
 });
--- db-api/util/db.js
+++ db-api/util/db.js
@@ -1,19 +1,19 @@
 const db = require('mysql2');
 
-// const dbPool = db.createPool({
-//   host: 'tourey-server-dev.c2ncyvtifq7i.us-east-1.rds.amazonaws.com',
-//   port: 3306,
-//   user: 'admin',
-//   database: 'tourney_server',
-//   password: '44gRmEvP9xLhR6d',
-// });
 const dbPool = db.createPool({
-  host: 'localhost',
-  port: 3307,
+  host: 'tourey-server-dev.c2ncyvtifq7i.us-east-1.rds.amazonaws.com',
+  port: 3306,
   user: 'admin',
   database: 'tourney_server',
   password: '44gRmEvP9xLhR6d',
 });
+// const dbPool = db.createPool({
+//   host: 'localhost',
+//   port: 3307,
+//   user: 'admin',
+//   database: 'tourney_server',
+//   password: '44gRmEvP9xLhR6d',
+// });
 
 // const dbPool = db.createPool({
 //   host: 'localhost',
