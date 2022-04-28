3428ed9a128e667f433b71e5ca16136e42c9da44
moose918 <gumpu.musa@gmail.com>
2022-04-28 14:10:06 +0200

    Fixed tournament

--- db-api/controllers/tournamentController.js
+++ db-api/controllers/tournamentController.js
@@ -1,7 +1,21 @@
 const db = require('../util/db');
 const dbErrorLogger = require('../util/dbErrorLogger');
 
-exports.getTournaments = async (req, res) => {};
+exports.getTournaments = async (req, res) => {
+  const GET_ALL_TOURNAMENTS = 'SELECT * FROM `TOURNAMENTS`';
+
+  await db.execute(GET_ALL_TOURNAMENTS)
+  .then(([rows, fields]) => {
+    res.status(201).json({
+      status: 'success',
+      message: 'Retrieved tournaments successfully',
+      newGame: rows,
+    });
+  })
+  .catch((err) => {
+    dbErrorLogger(res, err, 'Unable to add the tournament');
+  });
+};
 exports.insertTournament = async (req, res) => {
   const clientInput = req.body;
   const INSERT_TOURNAMENT = `CALL insert_tournament("${clientInput.tournamentName}", "${clientInput.gameID}")`;
--- db-api/routes/tournamentRouter.js
+++ db-api/routes/tournamentRouter.js
@@ -4,7 +4,7 @@ const tournamentController = require('../controllers/tournamentController');
 
 const router = express.Router();
 
-router.route('/').post(tournamentController.getTournaments);
+router.route('/').get(tournamentController.getTournaments);
 router.route('/add').post(tournamentController.insertTournament);
 // router.route('/').delete(tournamentController.deleteTournament);
 
