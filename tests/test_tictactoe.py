2d33939aa14404ea6b24c8392d05e4bfd8b4b9bb
LordGregorious <lordgregorious@Stevie.localdomain>
2022-05-09 01:02:23 +0200

    more unit tests

--- tests/test_tictactoe.py
+++ tests/test_tictactoe.py
@@ -5,16 +5,19 @@ Created on Sun Apr 10 02:55:36 2022
 @author: Gregory Cowan
 """
 
-from tictactoe import Play_Move, board
+from tictactoe import Play_Move, board, Simulate_Server_Message
 
 def test_making_move():
-    Play_Move('1,1_x')
-    assert board[1][1] == 'x'
-    Play_Move('0,1_o')
-    assert board[0][1] == 'o'
+    Play_Move('1 1 X')
+    assert board[1][1] == 'X'
+    Play_Move('0 1 O')
+    assert board[0][1] == 'O'
     
 def test_board_setup():
     assert board[0][0] == ' '
     
 def test_board_size():
     assert board.shape == (3,3)
+    
+def test_receive_move():
+    assert Simulate_Server_Message('move') != '' 
--- tests/tictactoe.py
+++ tests/tictactoe.py
@@ -12,8 +12,57 @@ board = np.array([[' ', ' ', ' '],
                   [' ', ' ', ' ']])
 
 def Play_Move(player_move):
-    row = int(player_move[: player_move.find(',')])
-    col = int(player_move[player_move.find(',') + 1 : player_move.find('_')])
-    piece = player_move[player_move.find('_') + 1:]
+    data = player_move.split(' ')
+    try:
+        row = int(data[0])
+        col = int(data[1])
+    except:
+        print(player_move)
+    piece = data[2]
     board[row][col] = piece
     print(board)
+    
+def Find_Move():
+    available_squares = []
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == ' ':
+                available_squares.append((i,j))
+      
+    # method shouldn't be running when board is full            
+    n = np.random.randint(0, len(available_squares))
+    return available_squares[n]
+
+def Simulate_Server_Message(move):
+    print('Player 1 is ready for game.')
+    
+    # flag used to determine if agent is X or O
+    flag = True
+    piece = 'X'
+    
+    if move == 'move':
+        # this agent is going first so it will be X
+        if flag:
+            flag = False
+            
+        random_move = Find_Move()
+        final_move = '{} {} {}'.format(random_move[0], random_move[1], piece)
+        Play_Move(final_move)
+        
+        return final_move
+    
+    elif move[:6] == 'winner':
+        print(move[7:] + ' won!')
+
+    elif move == 'draw':
+        print('Draw!')
+
+    else:
+        # this agent is going second so it will be O
+        if flag:
+            piece = 'O'
+            flag = False
+            
+        Play_Move(move)
+        
+    print('Game Over.')
