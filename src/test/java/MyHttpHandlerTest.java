329d90ac7ceabed93b95d97999ab5ed03cfff670
Gregory Cowan <greg21oct@gmail.com>
2022-05-23 00:22:56 +0200

    Render request

--- src/test/java/MyHttpHandlerTest.java
+++ src/test/java/MyHttpHandlerTest.java
@@ -1,3 +1,5 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -5,8 +7,10 @@ import static org.junit.Assert.assertNull;
 
 import java.io.IOException;
 
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class MyHttpHandlerTest {
     @Test
@@ -20,5 +24,42 @@ public class MyHttpHandlerTest {
         int responseCode = connection.getResponseCode();
 
         assertEquals(responseCode, 200);
+    }
+
+    @Test
+    public void testRenderRequest() throws IOException {
+        URL url = new URL("http://localhost:8001/game-server/images/game0/0.jpg");
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("POST");
+
+        // create JSON request body
+        String jsonBody = "{\"data\":{\"type\":\"render\",\"game\":\"Tic-Tac-Toe\",\"moves\":[\"0 0 X\",\"1 1 O\",\"0 1 X\",\"2 0 O\",\"0 2 X\"]},\"signal\":{}}";
+
+        /*JSONObject data = new JSONObject();
+        data.put("type", "render");
+        data.put("game", "Tic-Tac-Toe");
+
+        JSONArray moves = new JSONArray();
+        moves.add("0 0 X");
+        moves.add("1 1 O");
+
+        data.put("moves", moves);
+
+        JSONObject inputData = new JSONObject();
+        inputData.put("data", data);
+        inputData.put("signal", new JSONObject());*/
+
+        connection.setRequestProperty("Content-Type", "application/json; utf-8");
+        connection.setRequestProperty("Accept", "application/json");
+        connection.setDoOutput(true);
+
+        try(OutputStream os = connection.getOutputStream()) {
+            byte[] input = jsonBody.getBytes(StandardCharsets.UTF_8);
+            os.write(input, 0, input.length);
+        }
+
+        int responseCode = connection.getResponseCode();
+
+        assertEquals(responseCode, 400);
     }
 }
